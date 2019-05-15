7cbc1eb90fba2d5d11518c5dd2124e1fc9ec88c1
Vladimir Babenko <babenkovladimirbmd@gmail.com>
2019-05-15 20:44:34 +0300

    initial commit

---
+++ app/src/main/java/com/babenkovladimir/composite_application_x/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.babenkovladimir.composite_application_x
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/babenkovladimir/composite_application_x/rx/RxNavigatingActivity.kt
@@ -0,0 +1,12 @@
+package com.babenkovladimir.composite_application_x.rx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RxNavigatingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx_navigating)
+    }
+}
