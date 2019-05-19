60202af8cc84854ae7ff356c9e38f9c548b77b9f
Vladimir Babenko <babenkovladimirbmd@gmail.com>
2019-05-19 14:31:57 +0300

    add strategy type

--- app/src/main/java/com/babenkovladimir/composite_application_x/mvp/moxy_x_implementation/moxyx/IMoxyxPresenter.kt
+++ app/src/main/java/com/babenkovladimir/composite_application_x/mvp/moxy_x_implementation/moxyx/IMoxyxPresenter.kt
@@ -1,6 +1,13 @@
 package com.babenkovladimir.composite_application_x.mvp.moxy_x_implementation.moxyx
 
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
 interface IMoxyxPresenter {
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun incrementCount()
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun decrementCount()
 }
