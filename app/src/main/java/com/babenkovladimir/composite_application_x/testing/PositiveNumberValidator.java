deb473d0a56531d05702d76359b202bf2e75f03f
Vladimir Babenko <babenkovladimirbmd@gmail.com>
2019-08-28 08:21:19 +0300

    changes during lesson;

---
+++ app/src/main/java/com/babenkovladimir/composite_application_x/testing/PositiveNumberValidator.java
@@ -0,0 +1,8 @@
+package com.babenkovladimir.composite_application_x.testing;
+
+public class PositiveNumberValidator {
+
+    public boolean isPositive(int number) {
+        return number > 0;
+    }
+}
