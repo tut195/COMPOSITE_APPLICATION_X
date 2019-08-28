3d2c016ce745504d2c47d7f2e5b8b4ca0c942aed
Vladimir Babenko <babenkovladimirbmd@gmail.com>
2019-08-28 08:21:53 +0300

    Revert "changes during lesson;"
    This reverts commit deb473d0

--- app/src/main/java/com/babenkovladimir/composite_application_x/testing/PositiveNumberValidator.java
+++
@@ -1,8 +0,0 @@
-package com.babenkovladimir.composite_application_x.testing;
-
-public class PositiveNumberValidator {
-
-    public boolean isPositive(int number) {
-        return number > 0;
-    }
-}
