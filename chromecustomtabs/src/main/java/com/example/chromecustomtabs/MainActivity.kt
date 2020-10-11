9f7316aa6d52b949bcd8b1bbf08e45f7b3acc475
vladimir <babenkovladimirbmd@gmail.com>
2020-10-11 16:07:47 +0300

    add chrome custom tabs

--- chromecustomtabs/src/main/java/com/example/chromecustomtabs/MainActivity.kt
+++ chromecustomtabs/src/main/java/com/example/chromecustomtabs/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.chromecustomtabs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val url = "https://paul.kinlan.me/"
+
+        val builder = CustomTabsIntent.Builder()
+
+
+        //customTabsIntent.launchUrl(this, Uri.parse(url))
+
+
+        //Configure the color of the address bar
+        val colorInt = Color.parseColor("#FF0000") //red
+        builder.setToolbarColor(colorInt)
+
+//        Configure a custom action button
+
+        //builder.setActionButton(icon, description, pendingIntent, tint);
+
+
+        val customTabsIntent = builder.build()
+        chrmotebasBtn.setOnClickListener {
+            customTabsIntent.launchUrl(this, Uri.parse(url))
+        }
+
     }
 }
